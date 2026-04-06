871eaa147e1ce581a44a288f2246a32bf8fad3b5
Vamp_ <75939771+Vamp6969@users.noreply.github.com>
2026-04-06 12:11:02 +0530

    Update setup.sh to include package updates and cleanup
    Added commands to update package lists and autoremove unused packages.

--- setup.sh
+++ setup.sh
@@ -1,5 +1,7 @@
 # This script will install the necessary dependencies for Sentinel, including lolcat, figlet, and toilet.
+sudo apt update -y
 sudo apt install figlet lolcat -y
 # This script will also install neofetch and htop for system monitoring.
 apt-get install neofetch -y
 apt-get install htop -y
+sudo apt autoremove -y
